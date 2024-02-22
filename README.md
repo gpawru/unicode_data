647c669a319a024c6cfb6c92b9db1019c660237a
Anton <anton@yant.tech>
2024-02-22 12:07:21 +0300

    filtered_trie, removed implicit

--- README.md
+++ README.md
@@ -10,14 +10,12 @@
 
 ## UCA:
 
-* **DUCET_WEIGHTS**, **DUCET_IMPLICIT_WEIGHTS**: `Vec<WeightsEntry>` - allkeys.txt, DUCET (второй вариант - с вычисляемыми весами)
-* **DUCET**, **DUCET_IMPLICIT**: `HashMap<u32, TrieNode>` - бор, корневые элементы - одиночные кодпоинты, дочерние элементы для каждого уровня - N + 1 кодпоинтов в последовательности
+* **DUCET**: `Vec<WeightsEntry>` - allkeys.txt, DUCET
+* **DUCET_FILTERED_TRIE**: `HashMap<u32, TrieNode>` - DUCET в виде дерева, содержащая только NFD-кодпоинты
 * **COLLATION_TEST_DUCET_NON_IGNORABLE**, **COLLATION_TEST_DUCET_SHIFTED**: `Vec<CollationTest>` - CollationTest, тесты сопоставлений
 
 ## CLDR:
 
-* **CLDR_UND_WEIGHTS**, **CLDR_UND_IMPLICIT_WEIGHTS**: `Vec<WeightsEntry>` - allkeys_CLDR.txt, CLDR-версия DUCET
-* **CLDR_UND**, **CLDR_UND_IMPLICIT**: `HashMap<u32, TrieNode>` - аналогично **DUCET** / **DUCET_IMPLICIT**
+* **CLDR_UND**: `Vec<WeightsEntry>` - allkeys_CLDR.txt, CLDR-версия DUCET
+* **CLDR_FILTERED_TRIE**: `HashMap<u32, TrieNode>` - аналогично **DUCET_FILTERED_TRIE**
 * **COLLATION_TEST_CLDR_NON_IGNORABLE**, **COLLATION_TEST_CLDR_SHIFTED**: `Vec<CollationTest>` - CollationTest, тесты сопоставлений
-
-*также в библиотеке можно найти методы для получения вычисляемых весов: implicit_weights (общий случай), implicit_weights_tangut, implicit_weights_nushu, implicit_weights_khitan, implicit_weights_core_han, implicit_weights_other_han, implicit_weights_unassigned, применимые как для **DUCET** так и для **CLDR_UND**.*
--- src/parse/uca_cldr/allkeys.rs
+++ src/parse/uca_cldr/allkeys.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use crate::get_block_by_name;
+use crate::QC_NFD;
 
 /// запись таблицы DUCET / адаптированной таблицы DUCET для CLDR, полученная из allkeys.txt UCA / CLDR
 #[derive(Debug, Clone)]
@@ -9,7 +9,6 @@ pub struct WeightsEntry
     pub codes: Vec<u32>,
     pub weights: Vec<Weights>,
     pub description: String,
-    pub is_implicit: bool,
 }
 
 /// веса для кодпоинта из DUCET, 3 уровня
@@ -26,30 +25,26 @@ pub struct Weights
 #[derive(Debug, Clone)]
 pub struct TrieNode
 {
-    pub weights: &'static WeightsEntry,
+    pub weights: &'static Vec<Weights>,
     pub children: Option<HashMap<u32, TrieNode>>,
 }
 
 lazy_static! {
-    /// таблица DUCET
-    pub static ref DUCET: HashMap<u32, TrieNode> = weights_trie(&DUCET_WEIGHTS);
-    pub static ref DUCET_IMPLICIT: HashMap<u32, TrieNode> = weights_trie(&DUCET_IMPLICIT_WEIGHTS);
+    /// DUCET в виде дерева, содержащая только NFD-кодпоинты
+    pub static ref DUCET_FILTERED_TRIE: HashMap<u32, TrieNode> = weights_trie(&DUCET);
+    /// таблица DUCET из allkeys.txt
+    pub static ref DUCET: Vec<WeightsEntry> = allkeys(ALLKEYS_UCA);
 
-    pub static ref DUCET_WEIGHTS: Vec<WeightsEntry> = allkeys(ALLKEYS_UCA, false);
-    pub static ref DUCET_IMPLICIT_WEIGHTS: Vec<WeightsEntry> = allkeys(ALLKEYS_UCA, true);
-
-    /// таблица DUCET, адаптированная для CLDR
-    pub static ref CLDR_UND: HashMap<u32, TrieNode> = weights_trie(&CLDR_UND_WEIGHTS);
-    pub static ref CLDR_UND_IMPLICIT: HashMap<u32, TrieNode> = weights_trie(&CLDR_UND_IMPLICIT_WEIGHTS);
-
-    pub static ref CLDR_UND_WEIGHTS: Vec<WeightsEntry> = allkeys(ALLKEYS_CLDR, false);
-    pub static ref CLDR_UND_IMPLICIT_WEIGHTS: Vec<WeightsEntry> = allkeys(ALLKEYS_CLDR, true);
+    /// CLDR UND в виде дерева, содержащая только NFD-кодпоинты
+    pub static ref CLDR_UND_FILTERED_TRIE: HashMap<u32, TrieNode> = weights_trie(&CLDR_UND);
+    /// таблица DUCET из allkeys.txt, адаптированная для CLDR
+    pub static ref CLDR_UND: Vec<WeightsEntry> = allkeys(ALLKEYS_CLDR);
 }
 
 const ALLKEYS_UCA: &str = include_str!("./../../../data/uca 15.1.0/allkeys.txt");
 const ALLKEYS_CLDR: &str = include_str!("./../../../data/cldr 44/allkeys_CLDR.txt");
 
-fn allkeys(source: &str, include_implicit_weights: bool) -> Vec<WeightsEntry>
+fn allkeys(source: &str) -> Vec<WeightsEntry>
 {
     let mut allkeys = vec![];
 
@@ -104,223 +99,79 @@ fn allkeys(source: &str, include_implicit_weights: bool) -> Vec<WeightsEntry>
             codes,
             weights,
             description,
-            is_implicit: false,
         });
     }
 
-    if !include_implicit_weights {
-        return allkeys;
-    }
-
-    // добавим вычисляемые значения
-    // см. https://www.unicode.org/reports/tr10/tr10-49.html#Implicit_Weights
-
-    // примечание:
-    // UCD, Blocks.txt: Tangut Supplement: U+18D00 ..= U+18D7F.
-    // UCA, allkeys.txt: Tangut Supplement: U+18D00 ..= U+18D8F, что является очевидной ошибкой.
-    //
-    // кроме того, на текущий момент (15.1.0) последний заданный кодпоинт, принадлежащий этому блоку - U+18D08.
-    // так как за блоком Tangut Supplement следуют неназначенные кодпоинты - не имеет критического значения,
-    // по какой формуле для этих кодпоинтов будут рассчитываться веса.
-
-    macro_rules! block {
-        ($name: expr) => {
-            get_block_by_name($name).unwrap().range()
-        };
-        ($name: expr, assigned) => {
-            get_block_by_name($name)
-                .unwrap()
-                .range()
-                .filter(|c| crate::UNICODE.contains_key(c))
-        };
-    }
-
-    block!("Tangut", assigned).for_each(|c| allkeys.push(implicit_weights_tangut(c)));
-    block!("Tangut Components", assigned).for_each(|c| allkeys.push(implicit_weights_tangut(c)));
-    block!("Tangut Supplement", assigned).for_each(|c| allkeys.push(implicit_weights_tangut(c)));
-    block!("Nushu", assigned).for_each(|c| allkeys.push(implicit_weights_nushu(c)));
-    block!("Khitan Small Script", assigned).for_each(|c| allkeys.push(implicit_weights_khitan(c)));
-
-    // в allkeys.txt не упомянуты @implicitweights Han Unified Ideographs
-    //
-    // случай Core Han Unified Ideographs:
-    //  - отсутствуют веса для блока CJK Unified Ideographs,
-    //  - есть веса для Unified_Ideograph=True AND Block=CJK_Compatibility_Ideographs
-
-    block!("CJK Unified Ideographs").for_each(|c| allkeys.push(implicit_weights_core_han(c)));
-
-    // случай All other Han Unified Ideographs: веса отсутствуют. данный кейс включает в себя
-    // все кодпоинты CJK Unified Ideographs Extension A ..= H
-
-    ('A' ..= 'H').for_each(|c| {
-        let block = format!("CJK Unified Ideographs Extension {}", c);
-        block!(&block).for_each(|c| allkeys.push(implicit_weights_other_han(c)));
-    });
+    allkeys.sort_by_key(|e| e.codes.len());
 
     allkeys
-}
-
-/// вычисляемые веса
-#[inline]
-pub fn implicit_weights(code: u32) -> WeightsEntry
-{
-    // примечание - во всех таблицах веса для кодпоинтов CJK Compatibility Ideographs уже рассчитаны, не учитываем их здесь
-    match code {
-        0x4E00 ..= 0x9FFF => implicit_weights_core_han(code),
-        0x17000 ..= 0x18AFF | 0x18D00 ..= 0x18D08 => implicit_weights_tangut(code),
-        0x1B170 ..= 0x1B2FF => implicit_weights_nushu(code),
-        0x18B00 ..= 0x18CFF => implicit_weights_khitan(code),
-        0x3400 ..= 0x4DBF | 0x20000 ..= 0x2A6DF | 0x2A700 ..= 0x2EE5F | 0x30000 ..= 0x323AF => {
-            implicit_weights_other_han(code)
-        }
-        _ => implicit_weights_unassigned(code),
-    }
-}
-
-#[inline]
-fn compose_implicit_weights(code: u32, aaaa: u16, bbbb: u16, description: &str) -> WeightsEntry
-{
-    WeightsEntry {
-        codes: vec![code],
-        weights: vec![
-            Weights {
-                l1: aaaa,
-                l2: 0x0020,
-                l3: 0x0002,
-                is_variable: false,
-            },
-            Weights {
-                l1: bbbb,
-                l2: 0,
-                l3: 0,
-                is_variable: false,
-            },
-        ],
-        description: description.to_owned(),
-        is_implicit: true,
-    }
-}
-
-#[inline]
-pub fn implicit_weights_tangut(code: u32) -> WeightsEntry
-{
-    compose_implicit_weights(
-        code,
-        0xFB00,
-        ((code - 0x17000) | 0x8000) as u16,
-        "Tangut codepoint",
-    )
-}
-
-#[inline]
-pub fn implicit_weights_nushu(code: u32) -> WeightsEntry
-{
-    compose_implicit_weights(
-        code,
-        0xFB01,
-        ((code - 0x1B170) | 0x8000) as u16,
-        "Nushu codepoint",
-    )
-}
-
-#[inline]
-pub fn implicit_weights_khitan(code: u32) -> WeightsEntry
-{
-    compose_implicit_weights(
-        code,
-        0xFB02,
-        ((code - 0x18B00) | 0x8000) as u16,
-        "Khitan codepoint",
-    )
-}
-
-#[inline]
-pub fn implicit_weights_core_han(code: u32) -> WeightsEntry
-{
-    compose_implicit_weights(
-        code,
-        (0xFB40 + (code >> 15)) as u16,
-        ((code & 0x7FFF) | 0x8000) as u16,
-        "Core Han Unified Ideograph",
-    )
-}
-
-#[inline]
-pub fn implicit_weights_other_han(code: u32) -> WeightsEntry
-{
-    compose_implicit_weights(
-        code,
-        (0xFB80 + (code >> 15)) as u16,
-        ((code & 0x7FFF) | 0x8000) as u16,
-        "Other Han Unified Ideograph",
-    )
-}
-
-#[inline]
-pub fn implicit_weights_unassigned(code: u32) -> WeightsEntry
-{
-    compose_implicit_weights(
-        code,
-        (0xFBC0 + (code >> 15)) as u16,
-        ((code & 0x7FFF) | 0x8000) as u16,
-        "Unassigned",
-    )
 }
 
 #[inline]
 fn weights_trie(table: &'static Vec<WeightsEntry>) -> HashMap<u32, TrieNode>
 {
-    table
-        .iter()
-        .filter(|w| w.codes.len() == 1)
-        .map(|w| {
-            (
-                w.codes[0],
-                trie_node_children(w, &table.iter().filter(|w| w.codes.len() > 1).collect()),
-            )
-        })
-        .collect()
-}
+    let mut roots = HashMap::new();
 
-#[inline]
-fn trie_node_children(
-    parent: &'static WeightsEntry,
-    source: &Vec<&'static WeightsEntry>,
-) -> TrieNode
-{
-    let sequence = &parent.codes;
+    for entry in table.iter() {
+        let codes = &entry.codes;
 
-    let all_children: Vec<&WeightsEntry> = source
-        .iter()
-        .filter(|n| n.codes.len() > sequence.len() && n.codes.starts_with(&sequence))
-        .map(|e| *e)
-        .collect();
+        // в последовательности есть ненормализованный кодпоинт - убираем
+        if codes.iter().any(|&code| match QC_NFD.get(code as usize) {
+            Some(&v) => v == 'N',
+            None => false,
+        }) {
+            continue;
+        }
 
-    let current_node_children: Vec<&WeightsEntry> = all_children
-        .iter()
-        .filter(|n| n.codes.len() == sequence.len() + 1)
-        .map(|e| *e)
-        .collect();
+        match codes.len() {
+            1 => {
+                roots.insert(
+                    codes[0],
+                    TrieNode {
+                        weights: &entry.weights,
+                        children: None,
+                    },
+                );
+            }
+            2 => {
+                roots.entry(codes[0]).and_modify(|root| {
+                    if root.children.is_none() {
+                        root.children = Some(HashMap::new());
+                    }
 
-    if current_node_children.is_empty() {
-        return TrieNode {
-            weights: parent,
-            children: None,
-        };
+                    root.children.as_mut().unwrap().insert(
+                        codes[1],
+                        TrieNode {
+                            weights: &entry.weights,
+                            children: None,
+                        },
+                    );
+                });
+            }
+            3 => {
+                roots.entry(codes[0]).and_modify(|root| {
+                    root.children
+                        .as_mut()
+                        .unwrap()
+                        .entry(codes[1])
+                        .and_modify(|second| {
+                            if second.children.is_none() {
+                                second.children = Some(HashMap::new());
+                            }
+
+                            second.children.as_mut().unwrap().insert(
+                                codes[2],
+                                TrieNode {
+                                    weights: &entry.weights,
+                                    children: None,
+                                },
+                            );
+                        });
+                });
+            }
+            _ => unreachable!(),
+        }
     }
 
-    let children = current_node_children
-        .iter()
-        .map(|w| {
-            (
-                *w.codes.last().unwrap(),
-                trie_node_children(w, &all_children),
-            )
-        })
-        .collect();
-
-    TrieNode {
-        weights: parent,
-        children: Some(children),
-    }
+    roots
 }
